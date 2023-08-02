f6519faee3aac507315de29f32a96460954f9336
Akhand2021 <apsraghuvanshi85@gmail.com>
2023-08-03 03:15:51 +0530

    commit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\v1\CompleteTaskController;
+use App\Http\Controllers\Api\v1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::prefix('v1')->group(function () {
+    Route::apiResource('/task', TaskController::class);
+    Route::patch('/task/{task}/complete', CompleteTaskController::class);
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
